599556315494da09e638606cf79a4b68cb1ce261
David Espinoza <despinoza1027@gmail.com>
2017-04-03 22:19:15 -0500

    created circles and css, only one circle will light up

---
+++ day-16-multi-toggle-on-click/script.js
@@ -0,0 +1,23 @@
+var names = document.querySelector('.names');
+var kitties = ['Rocky', 'Henry', 'Tasuki', 'Kairi', 'Kefka'];
+
+
+
+var newItem = document.createElement('li');
+
+
+for (var i = 0; i < kitties.length; i++) {
+
+  var newItem = document.createElement('li');
+
+  newItem.textContent = kitties[i];
+
+  names.appendChild(newItem);
+
+};
+
+
+
+names.addEventListener('click', function() {
+  newItem.classList.toggle('toggled');
+});

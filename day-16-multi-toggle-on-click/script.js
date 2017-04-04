23713facde5ad8b85b3e49b5e283a4a6294d84d5
David Espinoza <despinoza1027@gmail.com>
2017-04-04 10:42:50 -0500

    added button to index.html, added highlighting functionality to created buttons'

--- day-16-multi-toggle-on-click/script.js
+++ day-16-multi-toggle-on-click/script.js
@@ -2,26 +2,24 @@ var names = document.querySelector('.names');
 var kitties = ['Rocky', 'Henry', 'Tasuki', 'Kairi', 'Kefka'];
 
 for (var i = 0; i < kitties.length; i++) {
-
   var newItem = document.createElement('li');
-
   newItem.textContent = kitties[i];
-
   names.appendChild(newItem);
-
-  newItem.addEventListener('click', function(evt) {
-    evt.target.classList.toggle('toggled');
-  });
-
 };
-
 
 var theButton = document.querySelector('#add-thing');
 var ol = document.querySelector('ol');
 
+ol.addEventListener('click', function(evt) {
+  // console.log(evt);
+  if (evt.target.tagName === 'LI') {
+    evt.target.classList.toggle('toggled');
+  }
+});
+
 theButton.addEventListener('click', function() {
   var li = document.createElement('li');
-  li.textContent = 'new';
+  li.textContent = '=^.^=';
 
   ol.appendChild(li);
 });

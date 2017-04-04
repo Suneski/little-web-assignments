4b9bae852922f0cd268c1327d28f53fc39be7bb7
David Espinoza <despinoza1027@gmail.com>
2017-04-04 10:06:52 -0500

    uploaded day-17-warmup folder

---
+++ day-17-warmup/script.js
@@ -0,0 +1,65 @@
+
+var groceries = [
+  { name: 'milk', cost: 5.20 },
+  { name: 'ribeye', cost: 12.78 },
+  { name: 'gum', cost: 1.12 },
+  { name: 'flowers', cost: 7.99 },
+  { name: 'pork chops', cost: 8.50 },
+  { name: 'eggs', cost: 3.40 },
+  { name: 'Dr Pepper', cost: 0.99 },
+  { name: 'lip balm', cost: 0.99 },
+  { name: 'tissues', cost: 1.30 },
+  { name: 'toothbrush', cost: 3.00 },
+  { name: 'Claritin', cost: 12.36 },
+  { name: 'Tic-Tacs', cost: 1.85 }
+];
+
+
+//Make this function calculate and return the average cost.
+function averageCost(items) {
+
+  var sum = 0;
+
+  for (i = 0; i < items.length; i++) {
+    sum += groceries[i].cost;
+  }
+
+  return sum / items.length;
+}
+var average = averageCost(groceries);
+console.log('average', average);
+
+
+//Make this function return the most expensive item.
+function mostExpensive(items) {
+
+  var mostExpensiveItem = items[0];
+
+  for (i = 1; i < items.length; i++) {
+    if (items[i].cost > mostExpensiveItem.cost) {
+      mostExpensiveItem = items[i];
+    }
+  }
+
+  return mostExpensiveItem;
+}
+
+var mostExpensiveItem = mostExpensive(groceries);
+console.log('most expensive', mostExpensiveItem);
+
+
+//Make this function return the least expensive item.
+function leastExpensive(items) {
+  var leastExpensiveItem = items[0];
+
+  for (i = 0; i < items.length; i++) {
+    if (items[i].cost < leastExpensiveItem.cost) {
+      leastExpensiveItem = items[i];
+    }
+  }
+
+  return leastExpensiveItem;
+}
+
+var leastExpensiveItem = leastExpensive(groceries);
+console.log('least expensive', leastExpensiveItem);

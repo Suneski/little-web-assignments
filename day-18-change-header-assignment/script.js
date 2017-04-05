bcf1c6b2c24de5869dbb9ca1ee3085af37ace6f6
David Espinoza <despinoza1027@gmail.com>
2017-04-05 15:43:05 -0500

    created day-18-change-header-assignment folder with initial files

---
+++ day-18-change-header-assignment/script.js
@@ -0,0 +1,19 @@
+// var box = document.getElementById('special-div');
+// var isHidden = false;
+//
+// window.addEventListener('scroll', function() {
+//   if (window.scrollY > 1000 && isHidden === false) {
+//     // box.classList.add("bye");
+//     box.style.display = 'none';
+//     isHidden = true;
+//   }
+//   else if (window.scrollY <= 1000 && isHidden === true) {
+//     // box.classList.remove("bye");
+//     box.style.display = 'block';
+//     isHidden = false;
+//   }
+// });
+//
+// window.addEventListener('resize', function () {
+//   console.log('resizing');
+// });

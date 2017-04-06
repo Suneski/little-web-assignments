450bf58fa7c73d7f7f216e377bd1941c54c39686
David Espinoza <despinoza1027@gmail.com>
2017-04-05 19:47:49 -0500

    created site template, included js

--- day-18-change-header-assignment/script.js
+++ day-18-change-header-assignment/script.js
@@ -1,13 +1,33 @@
 var nav = document.querySelector('nav');
+var teamRocket = document.querySelector('.team-rocket');
+var red = document.querySelector('.red')
+
 var topOfPage = true;
 
 window.addEventListener('scroll', function() {
-  if (window.scrollY > 200) {
-    nav.style.backgroundColor = 'green';
-    topOfPage = false;
-  }
-  else if (window.scrollY <= 200) {
-    nav.style.backgroundColor = 'blue';
+  console.log(window.scrollY);
+
+  if (window.scrollY < 100 && topOfPage === false) {
+    nav.style.backgroundColor = 'transparent';
+    teamRocket.style.display = 'none';
     topOfPage = true;
   }
+
+  else if (window.scrollY > 100 && window.scrollY <= 800 && topOfPage === true) {
+    nav.style.backgroundColor = 'rgba(0, 0, 0, 0.41)';
+    teamRocket.style.display = 'block';
+    red.style.display = 'none';
+    topOfPage = false;
+  }
+
+  else if (window.scrollY > 800 && topOfPage === false) {
+    teamRocket.style.display = 'none';
+    red.style.display = 'block';
+    topOfPage = true;
+  }
+
+  else if (window.scrollY < 800 && topOfPage === true) {
+    topOfPage = false;
+  }
+
 });

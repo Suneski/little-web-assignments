f6756f952cfbed13234720b78c1b87d87cc8110d
David Espinoza <despinoza1027@gmail.com>
2017-04-08 11:06:32 -0500

    created time-to-read folder, finalized files. combined assignments (estimated reading time & social media sprites_ )

---
+++ day-20-time-to-read/script.js
@@ -0,0 +1,21 @@
+var count = document.querySelector('body').innerHTML.split(' ').length;
+var time = Math.round(count / 230);
+
+document.querySelector("#estimated-read-time").innerHTML="(Estimated read time: " + time + ' minutes)';
+
+
+
+var social = document.querySelector('.social');
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+
+  if (window.scrollY >= 480) {
+    social.style.display='block';
+  }
+
+  else if (window.scrollY < 480) {
+    social.style.display='none';
+  }
+
+});

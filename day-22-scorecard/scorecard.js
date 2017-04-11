0787b086ff4778407e7fefbcb38a7db03c0ea6fd
David Espinoza <despinoza1027@gmail.com>
2017-04-11 14:53:10 -0500

    updated scorecard.js, alternating chalk colors per line

--- day-22-scorecard/scorecard.js
+++ day-22-scorecard/scorecard.js
@@ -1,16 +1,22 @@
+var chalk = require('chalk');
+
 var fs = require('fs');
 
 fs.readFile('Most-Recent-Cohorts-Scorecard-Elements.csv', 'utf-8', function(err, data) {
-  // console.log('async error', err)
-  // console.log('async data', data);
 
   var lines = data.split('\n');
-  // console.log('lines', lines);
 
   for (var i = 1; i < lines.length - 1; i++) {
     var line = lines[i];
     var pieces = line.split(',');
-    console.log('name -', pieces[3]);
+
+    if (i % 2 === 0) {
+      console.log(chalk.cyan.bold(pieces[3]), '||', chalk.cyan('ACT cumulative score (75th percentile):', pieces[32]));
+    }
+    else {
+      console.log(chalk.magenta.bold(pieces[3]), '||', chalk.magenta('ACT cumulative score (75th percentile):', pieces[32]));
+    }
+
   }
 
 });

f49ffa3ad7e11422e82f2b702c6f06f3a2b8630d
David Espinoza <despinoza1027@gmail.com>
2017-04-11 14:27:00 -0500

    added general search bar to project

--- day-21-github-users/script.js
+++ day-21-github-users/script.js
@@ -1,32 +1,43 @@
 var users = document.querySelector('#users');
+var searchQuery = document.querySelector('#search-query');
 
 
-var promise = $.ajax({
-  url: 'https://api.github.com/search/users?q=robot'
-});
+searchQuery.addEventListener('keyup', function(evt){
 
-
-promise.done(function(data) {
-
-  for (var i = 0; i < data.items.length; i++){
-    var anLi = document.createElement('li');
-
-    var login = document.createElement('h2');
-    login.textContent = data.items[i].login;
-    anLi.appendChild(login);
-
-    var profile = document.createElement('a');
-    // profile.textContent = 'Github Profile';
-    profile.href = data.items[i].html_url;
-    anLi.appendChild(profile);
-
-    var image = document.createElement('img');
-    image.src = data.items[i].avatar_url;
-    profile.appendChild(image);
-
-
-
-
-    users.appendChild(anLi);
+  if (evt.keyCode !== 13) {
+    return;
   }
+
+  users.innerHTML = '';
+
+  var promise = $.ajax({
+    url: 'https://api.github.com/search/users?q=' + searchQuery.value
+  });
+
+
+  promise.done(function(data) {
+
+    for (var i = 0; i < data.items.length; i++){
+      var anLi = document.createElement('li');
+
+      var login = document.createElement('h2');
+      login.textContent = data.items[i].login;
+      anLi.appendChild(login);
+
+      var profile = document.createElement('a');
+      // profile.textContent = 'Github Profile';
+      profile.href = data.items[i].html_url;
+      anLi.appendChild(profile);
+
+      var image = document.createElement('img');
+      image.src = data.items[i].avatar_url;
+      profile.appendChild(image);
+
+
+
+
+      users.appendChild(anLi);
+    }
+  });
+
 });

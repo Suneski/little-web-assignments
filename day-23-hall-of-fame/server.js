5a17351048470b2d629431f60bf9f9b10864182a
David Espinoza <despinoza1027@gmail.com>
2017-04-12 11:09:29 -0500

    inlcuded sodas api on server.js

--- day-23-hall-of-fame/server.js
+++ day-23-hall-of-fame/server.js
@@ -16,6 +16,20 @@ app.get('/api/favoritefoods', function(req, res) {
 
 
 
+app.get('/api/favoritesodas', function(req,res) {
+
+  console.log('Calling sodas api');
+
+  var output = {
+    sodas: ['ramune', 'root beer', 'Dr. Pepper', 'grape soda', 'that orange one']
+  }
+
+  res.send(output);
+
+});
+
+
+
 
 
 app.listen(7690, function() {

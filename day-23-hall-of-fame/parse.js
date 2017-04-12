04ce76e96b16845b5b5bb6d63571327c94156772
David Espinoza <despinoza1027@gmail.com>
2017-04-12 12:17:54 -0500

    updated parse.js to include player data

--- day-23-hall-of-fame/parse.js
+++ day-23-hall-of-fame/parse.js
@@ -1,18 +1,28 @@
-var chalk = require('chalk');
-
 var fs = require('fs');
 
 fs.readFile('HallOfFame.csv', 'utf-8', function(err, data) {
 
   var lines = data.split('\n');
 
-  for (var i = 0; i < lines.length; i++) {
-    var line = lines[i];
-    var pieces = line.split(',');
+  var output = [];
 
-    if (pieces[1] === '2016' && pieces[5] > 200) {
-      console.log(chalk.cyan.bold('Player ID ') + chalk.cyan(pieces[0]));
-    }
+  for (var i = 0; i < lines.length; i++) {
+    var values = lines[i].split(',');
+
+    var person = values[0];
+    var year = values[1];
+    var votes =values[5];
+
+    output.push({
+      personID: person,
+      year: year,
+      votes: votes
+    });
+
+
   }
+
+
+  console.log(output);
 
 });

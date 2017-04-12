d34d4ee6fbf186918f8c1b30d55144c81b65c795
David Espinoza <despinoza1027@gmail.com>
2017-04-12 14:09:16 -0500

    added previous/next buttons to search

--- day-21-github-users/script.js
+++ day-21-github-users/script.js
@@ -1,21 +1,25 @@
 var users = document.querySelector('#users');
 var searchQuery = document.querySelector('#search-query');
+var nextButton = document.querySelector('#next-button');
+var previousButton = document.querySelector('#previous-button');
+
+var page = 1;
+var totalResults;
+var pageCount;
 
 
-searchQuery.addEventListener('keyup', function(evt){
-
-  if (evt.keyCode !== 13) {
-    return;
-  }
-
+function makeAjaxCall() {
   users.innerHTML = '';
 
   var promise = $.ajax({
-    url: 'https://api.github.com/search/users?q=' + searchQuery.value
+    url: 'https://api.github.com/search/users?q=' + searchQuery.value + '&page=' + page
   });
 
 
   promise.done(function(data) {
+
+    totalResults = data.total_count;
+    pageCount = Math.ceil(totalResults / 30);
 
     for (var i = 0; i < data.items.length; i++){
       var anLi = document.createElement('li');
@@ -38,6 +42,58 @@ searchQuery.addEventListener('keyup', function(evt){
 
       users.appendChild(anLi);
     }
+
+    if (page === 1) {
+      previousButton.style.display = 'none';
+    }
+    else {
+      previousButton.style.display = 'inline';
+    }
+
+    if (page >= pageCount) {
+      nextButton.style.display = 'none';
+    }
+    else {
+      nextButton.style.display = 'inline';
+    }
+
+
   });
+
+
+
+}
+
+
+searchQuery.addEventListener('keyup', function(evt){
+
+  if (evt.keyCode !== 13) {
+    return;
+  }
+
+  makeAjaxCall();
+
+});
+
+
+
+nextButton.addEventListener('click', function() {
+
+  //increment number
+  page += 1;
+
+  //do ajax call again
+  makeAjaxCall();
+
+});
+
+
+previousButton.addEventListener('click', function() {
+
+  //decrease number
+  page -= 1;
+
+  //do ajax call again
+  makeAjaxCall();
 
 });

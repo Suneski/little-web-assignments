d426a9cbc06ee733ee69ead1d8a40189683ee2a0
David Espinoza <despinoza1027@gmail.com>
2017-04-12 12:19:15 -0500

    updated player data section

--- day-23-hall-of-fame/server.js
+++ day-23-hall-of-fame/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.use(express.static('public'));
@@ -47,6 +48,38 @@ app.get('/api/favoritesodas', function(req,res) {
 
   res.send(output);
 
+});
+
+
+
+app.get('/api/halloffame', function(req,res) {
+
+  console.log('Calling hall of fame api');
+
+  fs.readFile('HallOfFame.csv', 'utf-8', function(err, data) {
+
+    var lines = data.split('\n');
+
+    var output = [];
+
+    for (var i = 0; i < lines.length; i++) {
+      var values = lines[i].split(',');
+
+      var person = values[0];
+      var year = values[1];
+      var votes =values[5];
+
+      output.push({
+        personID: person,
+        year: year,
+        votes: votes
+      });
+    }
+
+    res.send({
+      stats: output
+    });
+  });
 });
 
 

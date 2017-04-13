19a9b2c46e9a8898caacc71d0c5e50737d865305
David Espinoza <despinoza1027@gmail.com>
2017-04-13 08:36:21 -0500

    updated page to include highlighted previous/next buttons

--- day-22-github-repo-search/script.js
+++ day-22-github-repo-search/script.js
@@ -1,21 +1,35 @@
 var repos = document.querySelector('#repos');
 var searchQuery = document.querySelector('#search-query');
+var nextButton = document.querySelector('#next-button');
+var previousButton = document.querySelector('#previous-button');
+var navBar = document.querySelector('nav');
+var searchResults = document.querySelector('.search-results');
+var totalResults = document.querySelector('.total-results');
 
+var page = 1;
+var totalResults;
+var pageCount;
 
-searchQuery.addEventListener('keyup', function(evt){
-
-  if (evt.keyCode !== 13) {
-    return;
-  }
+function makeAjaxCall() {
 
   repos.innerHTML = '';
 
   var promise = $.ajax({
-    url: 'https://api.github.com/search/repositories?q=' + searchQuery.value
+    url: 'https://api.github.com/search/repositories?q=' + searchQuery.value + '&page=' + page
   });
 
 
+
+  searchResults.textContent = 'Page: ' + page;
+  searchResults.style.display = 'inline-block';
+
+
+
+
   promise.done(function(data) {
+
+    totalResults = data.total_count;
+    pageCount = Math.ceil(totalResults / 30);
 
     for (var i = 0; i < data.items.length; i++){
       var anLi = document.createElement('li');
@@ -37,8 +51,78 @@ searchQuery.addEventListener('keyup', function(evt){
       userName.target = '_blank';
       anLi.appendChild(userName);
 
+
+
+
       repos.appendChild(anLi);
     }
+
+    if (page === 1) {
+      previousButton.style.display = 'inline';
+      previousButton.style.color = 'grey';
+    }
+    else {
+      previousButton.style.color = 'rgba(255, 0, 0, 0.6)';
+
+    }
+
+    if (page >= pageCount) {
+
+      nextButton.style.color = 'grey';
+    }
+    else {
+      nextButton.style.display = 'inline';
+      nextButton.style.color = 'rgba(255, 0, 0, 0.6)';
+    }
+
   });
+
+}
+
+
+
+searchQuery.addEventListener('keyup', function(evt){
+
+  if (evt.keyCode !== 13) {
+    page = 1;
+    return;
+  }
+
+
+  makeAjaxCall();
+
+});
+
+
+
+
+nextButton.addEventListener('click', function() {
+
+  //increment number
+  page += 1;
+
+  if (page >= pageCount) {
+    page = pageCount;
+  }
+
+  //do ajax call again
+  makeAjaxCall();
+
+});
+
+
+
+
+previousButton.addEventListener('click', function() {
+
+  //decrease number
+  page -= 1;
+
+  if (page <= 1) {
+    page = 1;
+  }
+
+  //do ajax call again
+  makeAjaxCall();
 
 });

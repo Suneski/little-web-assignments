654a68b35066b9ee67a2fc58f4ebdc7d499f557b
David Espinoza <despinoza1027@gmail.com>
2017-04-14 16:15:22 -0500

    converted DOM Manipulation to mustache.js in characters and planets

--- day-21-star-wars/characters.js
+++ day-21-star-wars/characters.js
@@ -1,4 +1,5 @@
 var peopleList = document.querySelector('#people-list');
+var template = document.querySelector('#list-template').innerHTML;
 
 
 var promise = $.ajax({
@@ -7,60 +8,79 @@ var promise = $.ajax({
 
 
 promise.done(function(data) {
+
+  peopleList.innerHTML = '';
+
+  var totalHtml = '';
+
+
   for (var i = 0; i < data.results.length; i++){
-    var anLi = document.createElement('li');
 
-    // var div = document.createElement('div');
-    // div.classList.add("li-div");
+    var html = Mustache.render(template, {
+      name: data.results[i].name,
+      gender: data.results[i].gender,
+      birthYear: data.results[i].birth_year,
+      height: data.results[i].height,
+      mass: data.results[i].mass,
+      skin: data.results[i].skin_color,
+      eye: data.results[i].eye_color,
+      hair: data.results[i].hair_color
+    });
+
+    totalHtml += html;
 
 
-    var name = document.createElement('h2');
-    name.textContent = data.results[i].name;
-    name.classList.add("character-name");
-    anLi.appendChild(name);
-
-    var gender = document.createElement('div');
-    gender.className = "gender";
-    gender.textContent = 'Gender : ' + data.results[i].gender;
-    name.classList.add("character-gender");
-    anLi.appendChild(gender);
-
-    var birthYear = document.createElement('div');
-    birthYear.className = 'birth-year';
-    birthYear.textContent = 'Birth Year : ' + data.results[i].birth_year;
-    birthYear.classList.add("birthYear");
-    anLi.appendChild(birthYear);
-
-    var height = document.createElement('div');
-    height.className = 'height';
-    height.textContent = 'Height : ' + data.results[i].height + ' cm';
-    height.classList.add("height");
-    anLi.appendChild(height);
-
-    var mass = document.createElement('div');
-    mass.className = 'mass';
-    mass.textContent = 'Mass : ' + data.results[i].mass + ' kg';
-    mass.classList.add("mass");
-    anLi.appendChild(mass);
-
-    var skin = document.createElement('div');
-    skin.className = 'skin';
-    skin.textContent = 'Skin Color : ' + data.results[i].skin_color;
-    skin.classList.add("skin");
-    anLi.appendChild(skin);
-
-    var eye = document.createElement('div');
-    eye.className = 'eye';
-    eye.textContent = 'Eye Color : ' + data.results[i].eye_color;
-    eye.classList.add("eye");
-    anLi.appendChild(eye);
-
-    var hair = document.createElement('div');
-    hair.className = 'hair';
-    hair.textContent = 'Hair Color : ' + data.results[i].hair_color;
-    hair.classList.add("hair");
-    anLi.appendChild(hair);
-
-    peopleList.appendChild(anLi);
+    // var anLi = document.createElement('li');
+    //
+    // var name = document.createElement('h2');
+    // name.textContent = data.results[i].name;
+    // name.classList.add("character-name");
+    // anLi.appendChild(name);
+    //
+    // var gender = document.createElement('div');
+    // gender.className = "gender";
+    // gender.textContent = 'Gender : ' + data.results[i].gender;
+    // name.classList.add("character-gender");
+    // anLi.appendChild(gender);
+    //
+    // var birthYear = document.createElement('div');
+    // birthYear.className = 'birth-year';
+    // birthYear.textContent = 'Birth Year : ' + data.results[i].birth_year;
+    // birthYear.classList.add("birthYear");
+    // anLi.appendChild(birthYear);
+    //
+    // var height = document.createElement('div');
+    // height.className = 'height';
+    // height.textContent = 'Height : ' + data.results[i].height + ' cm';
+    // height.classList.add("height");
+    // anLi.appendChild(height);
+    //
+    // var mass = document.createElement('div');
+    // mass.className = 'mass';
+    // mass.textContent = 'Mass : ' + data.results[i].mass + ' kg';
+    // mass.classList.add("mass");
+    // anLi.appendChild(mass);
+    //
+    // var skin = document.createElement('div');
+    // skin.className = 'skin';
+    // skin.textContent = 'Skin Color : ' + data.results[i].skin_color;
+    // skin.classList.add("skin");
+    // anLi.appendChild(skin);
+    //
+    // var eye = document.createElement('div');
+    // eye.className = 'eye';
+    // eye.textContent = 'Eye Color : ' + data.results[i].eye_color;
+    // eye.classList.add("eye");
+    // anLi.appendChild(eye);
+    //
+    // var hair = document.createElement('div');
+    // hair.className = 'hair';
+    // hair.textContent = 'Hair Color : ' + data.results[i].hair_color;
+    // hair.classList.add("hair");
+    // anLi.appendChild(hair);
+    //
+    // peopleList.appendChild(anLi);
   }
+
+  peopleList.innerHTML = totalHtml;
 });
--- day-21-star-wars/planets.js
+++ day-21-star-wars/planets.js
@@ -1,4 +1,5 @@
 var planetsList = document.querySelector('#planets-list');
+var template = document.querySelector('#list-template').innerHTML;
 
 
 var promise = $.ajax({
@@ -7,45 +8,67 @@ var promise = $.ajax({
 
 
 promise.done(function(data) {
+
+  planetsList.innerHTML = '';
+
+  var totalHtml = '';
+
   for (var i = 0; i < data.results.length; i++){
-    var anLi = document.createElement('li');
+
+    var html = Mustache.render(template, {
+      name: data.results[i].name,
+      population: data.results[i].population,
+      climate: data.results[i].climate,
+      terrain: data.results[i].terrain,
+      orbitalPeriod: data.results[i].orbital_period,
+      diameter: data.results[i].diameter,
+      gravity: data.results[i].gravity
+    });
+
+    totalHtml += html;
 
 
-    var name = document.createElement('h2');
-    name.textContent = data.results[i].name;
-    name.classList.add("planet-name");
-    anLi.appendChild(name);
 
-    var population = document.createElement('div');
-    population.textContent = 'Population : ' + data.results[i].population;
-    population.classList.add("population");
-    anLi.appendChild(population);
-
-    var climate = document.createElement('div');
-    climate.textContent = 'Climate : ' + data.results[i].climate;
-    climate.classList.add("climate");
-    anLi.appendChild(climate);
-
-    var terrain = document.createElement('div');
-    terrain.textContent = 'Terrain : ' + data.results[i].terrain;
-    terrain.classList.add("terrain");
-    anLi.appendChild(terrain);
-
-    var orbitalPeriod = document.createElement('div');
-    orbitalPeriod.textContent = 'Orbital Period : ' + data.results[i].orbital_period + ' days';
-    orbitalPeriod.classList.add("orbital-period");
-    anLi.appendChild(orbitalPeriod);
-
-    var diameter = document.createElement('div');
-    diameter.textContent = 'Diameter : ' + data.results[i].diameter + 'km';
-    diameter.classList.add("diameter");
-    anLi.appendChild(diameter);
-
-    var gravity = document.createElement('div');
-    gravity.textContent = 'Gravity : ' + data.results[i].gravity;
-    gravity.classList.add("gravity");
-    anLi.appendChild(gravity);
-
-    planetsList.appendChild(anLi);
+    // var anLi = document.createElement('li');
+    //
+    //
+    // var name = document.createElement('h2');
+    // name.textContent = data.results[i].name;
+    // name.classList.add("planet-name");
+    // anLi.appendChild(name);
+    //
+    // var population = document.createElement('div');
+    // population.textContent = 'Population : ' + data.results[i].population;
+    // population.classList.add("population");
+    // anLi.appendChild(population);
+    //
+    // var climate = document.createElement('div');
+    // climate.textContent = 'Climate : ' + data.results[i].climate;
+    // climate.classList.add("climate");
+    // anLi.appendChild(climate);
+    //
+    // var terrain = document.createElement('div');
+    // terrain.textContent = 'Terrain : ' + data.results[i].terrain;
+    // terrain.classList.add("terrain");
+    // anLi.appendChild(terrain);
+    //
+    // var orbitalPeriod = document.createElement('div');
+    // orbitalPeriod.textContent = 'Orbital Period : ' + data.results[i].orbital_period + ' days';
+    // orbitalPeriod.classList.add("orbital-period");
+    // anLi.appendChild(orbitalPeriod);
+    //
+    // var diameter = document.createElement('div');
+    // diameter.textContent = 'Diameter : ' + data.results[i].diameter + 'km';
+    // diameter.classList.add("diameter");
+    // anLi.appendChild(diameter);
+    //
+    // var gravity = document.createElement('div');
+    // gravity.textContent = 'Gravity : ' + data.results[i].gravity;
+    // gravity.classList.add("gravity");
+    // anLi.appendChild(gravity);
+    //
+    // planetsList.appendChild(anLi);
   }
+
+  planetsList.innerHTML = totalHtml;
 });

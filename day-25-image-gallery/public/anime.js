299d0ec4dc354b7b751caacf9c43f39127c644e4
David Espinoza <despinoza1027@gmail.com>
2017-04-16 12:42:33 -0500

    created anime page with css and js, finalized HW for now

---
+++ day-25-image-gallery/public/anime.js
@@ -0,0 +1,58 @@
+var h1 = document.querySelector('h1');
+var selectedImage = document.querySelector('.selected-image');
+var imageList = document.querySelector('.image-list');
+var template = document.querySelector('#list-template').innerHTML;
+var row = document.querySelector('.row');
+var thumbnailImage = document.querySelector('.thumbnail-image');
+var detailsDiv = document.querySelector('#details');
+var detailsTemplate = document.querySelector('#detail-template').innerHTML;
+
+var ajaxData;
+
+$.ajax({
+  url: '/api/animepics'
+})
+
+.done(function(data) {
+
+  ajaxData = data;
+
+  var html = '';
+
+  for (var i = 0; i < data.pictures.length; i++) {
+
+    html += Mustache.render(template, {
+      index: i,
+      imgThumbSrc: data.pictures[i].thumbnail,
+      fullSize: data.pictures[i].fullSize,
+      description: data.pictures[i].description
+    });
+
+  }
+
+  imageList.innerHTML = html;
+
+});
+
+
+imageList.addEventListener('click', function(evt) {
+  h1.style.display = 'none';
+
+  var index = Number(evt.target.getAttribute('data-index'));
+  // console.log(ajaxData.pictures[index]);
+
+  var html = Mustache.render(detailsTemplate, ajaxData.pictures[index]);
+  // console.log(html);
+
+  var previouslySelected = document.querySelector('.toggled');
+
+  if (previouslySelected !== null) {
+    previouslySelected.classList.remove('toggled');
+  }
+
+  if (evt.target.tagName === 'IMG') {
+    evt.target.classList.add('toggled');
+  }
+
+  detailsDiv.innerHTML = html;
+});

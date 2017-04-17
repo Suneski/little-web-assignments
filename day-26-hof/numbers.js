960d2fa218645f73380b5011fd7ce9899fe61345
David Espinoza <despinoza1027@gmail.com>
2017-04-17 14:56:34 -0500

    created folder and initial files

---
+++ day-26-hof/numbers.js
@@ -0,0 +1,69 @@
+
+var numbers = [1, 56, 89, 12, 45, 211, 45, 56, 32];
+
+// OLD SCHOOL LOOPING
+
+// function loopAndConsoleLog(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// loopAndConsoleLog(numbers)
+
+
+
+//Or, you can use the forEach function on arrays to do that
+
+// numbers.forEach(function(num) {
+//   console.log(num);
+// });
+
+
+
+
+
+// This is how you make forEach
+
+// function forEvery(arr, func) {
+//   for (var i = 0; i < arr.length; i++) {
+//     func(arr[i]);
+//   }
+// }
+//
+//
+// forEvery(numbers, function(num) {
+//   console.log(num);
+// });
+//
+// forEvery(['Bob', 'Frank'], function(name) {
+//   console.log('Hi', name);
+// });
+
+
+
+
+// All the numbers over 50
+
+// OLD WAY
+// var output = [];
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 50) {
+//     output.push(numbers[i]);
+//   }
+// }
+// console.log(output);
+
+
+// FOR EACH
+// numbers.forEach(function(num) {
+//   if (num > 50) {
+//     console.log(num);
+//   }
+// });
+
+var output = numbers.filter(function(num) {
+  return num > 50;
+});
+
+console.log(output);

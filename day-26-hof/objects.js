d160298e81930f6f576a188d65d5f3c6d72cc266
David Espinoza <despinoza1027@gmail.com>
2017-04-18 13:49:52 -0500

    uploaded objects.js in-class work

---
+++ day-26-hof/objects.js
@@ -0,0 +1,64 @@
+var peeps = [
+  { firstName: 'Jane', lastName: 'Jones', age: 12 },
+  { firstName: 'John', lastName: 'Smith', age: 18 },
+  { firstName: 'Fred', lastName: 'Johnson', age: 11 },
+  { firstName: 'Jaime', lastName: 'Oliver', age: 9 },
+  { firstName: 'Ragan', lastName: 'Perkinson', age: 1 },
+  { firstName: 'Chris', lastName: 'Smith', age: 5 },
+  { firstName: 'George', lastName: 'Jungle', age: 17 },
+  { firstName: 'Mike', lastName: 'Tyson', age: 16 },
+  { firstName: 'Jimmy', lastName: 'Smith', age: 21 },
+  { firstName: 'Chris', lastName: 'Thompson', age: 17 }
+];
+
+// // CONSOLE LOG OBJECT
+// peeps.forEach(function(obj) {
+//   console.log(obj);
+// });
+
+
+
+
+// // CONSOLE LOG FIRST NAME
+// peeps.forEach(function(obj) {
+//   console.log(obj.firstName);
+// });
+
+
+
+
+// // CONSOLE LOG ONLY IF YOUNGER THAN 15
+// var youngerThan15 = peeps.filter(function(obj) {
+//   return obj.age < 15;
+// });
+//
+// console.log('younger than 15', youngerThan15);
+//
+//
+// // CONSOLE LOG ONLY IF 15 OR OLDER
+// var fifteenAndOlder = peeps.filter(function(obj) {
+//   return obj.age >= 15;
+// });
+//
+// console.log('15 and older', fifteenAndOlder);
+
+
+
+
+// // CONSOLE LOG ONLY IF firstName === 'Chris'
+//
+// var onlyChris = peeps.filter(function(obj) {
+//   return obj.firstName === 'Chris';
+// });
+//
+// console.log('only Chris', onlyChris);
+
+
+
+
+// // CONSOLE LOG ONLY IF firstName has letter 'i';
+// var hasLetterI = peeps.filter(function(obj) {
+//   return obj.firstName.indexOf('i') > -1;
+// })
+//
+// console.log('Name contains the letter "i"', hasLetterI);

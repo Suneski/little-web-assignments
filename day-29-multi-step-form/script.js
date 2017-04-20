dfaf37e98c4ce230371ff32230bf7b6014f57d1e
David Espinoza <despinoza1027@gmail.com>
2017-04-20 14:25:34 -0500

    created folder and initial files, completed assignment requirements, needs styling

---
+++ day-29-multi-step-form/script.js
@@ -0,0 +1,60 @@
+var firstPage = document.querySelector('#page-1');
+var secondPage = document.querySelector('#page-2');
+var thirdPage = document.querySelector('#page-3');
+
+
+var firstNameInput = document.querySelector('#first-name');
+var lastNameInput = document.querySelector('#last-name');
+
+var nextButtonOne = document.querySelector('#next-page-one');
+
+var previousPage2 = document.querySelector('#previous-page-two');
+var nextPage2 = document.querySelector('#next-page-two');
+
+var previousPage3 = document.querySelector('#previous-page-three');
+var submit = document.querySelector('#submit');
+
+
+
+// PAGE 1
+
+nextButtonOne.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  if (firstNameInput.value === '' || lastNameInput.value === '') {
+    alert("ERROR! Please include both first AND last name!");
+  }
+  else {
+    firstPage.style.display = "none";
+    secondPage.style.display = "block";
+  }
+
+});
+
+// PAGE 2
+
+previousPage2.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  firstPage.style.display = "block";
+  secondPage.style.display = "none";
+
+});
+
+nextPage2.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  secondPage.style.display = "none";
+  thirdPage.style.display = "block";
+
+});
+
+// PAGE 3
+
+previousPage3.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  secondPage.style.display = "block";
+  thirdPage.style.display = "none";
+
+});

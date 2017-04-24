ae0bc51699339930aba07bba32f3f5b23096ef43
David Espinoza <despinoza1027@gmail.com>
2017-04-24 11:26:50 -0500

    got delimiter split to work

--- day-31-calculator/test/test.js
+++ day-31-calculator/test/test.js
@@ -1,13 +1,18 @@
-var assert = require('assert');
+  var assert = require('assert');
 
 var Calculator = {
   add: function(input) {
-    if(input.indexOf(',') > -1) {
-      var units = input.split(',');
+    if (input === '') {
+      return 0;
+    }
+    if (input.indexOf(",") > -1) {
+      var array = input.split(/[,]|[\n]+/);
       var sum = 0;
-      units.forEach(function(x) {
+
+      array.forEach(function(x) {
         sum += Number(x);
       });
+
       return sum;
     }
     return Number(input);
@@ -15,28 +20,110 @@ var Calculator = {
 }
 
 describe('String Calculator add method', function() {
-  it('should return 0 when the value is a blank string', function() {
+  it('should return 0 if the value is a blank string', function(){
     var result = Calculator.add('');
 
     assert.equal(0, result);
-  });
+  })
 
-  it('should return 3 when the value is 3', function() {
-    var result = Calculator.add('3');
+  it('should return 7 if the value is 7', function() {
+    var result = Calculator.add('7');
 
-    assert.equal(3, result);
-  });
-
-  it('should return 5 when the value is "2,3"', function() {
+    assert.equal(7, result);
+  })
+  it('should return 5 if the value is "2,3"', function(){
     var result = Calculator.add('2,3');
 
     assert.equal(5, result);
-  });
+  })
+  it('should return 12 if the value is "6,4,2"', function(){
+    var result = Calculator.add('6,4,2');
 
-  it('should return 10 when the value passed in is "5,5"', function() {
-    var result = Calculator.add('5,5');
-
-    assert.equal(10, result);
+    assert.equal(12, result);
   })
 
+  it('should return 6 if the value is “1\n2,3”', function() {
+    var result = Calculator.add("1\n2,3");
+
+    assert.equal(6, result);
+  })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var assert = require('assert');
+//
+// var Calculator = {
+//   add: function(input) {
+//     if(input.indexOf(',') > -1) {
+//       var units = input.split(',');
+//       var sum = 0;
+//       units.forEach(function(x) {
+//         sum += Number(x);
+//       });
+//       return sum;
+//     }
+//     return Number(input);
+//   }
+// }
+//
+// describe('String Calculator add method', function() {
+//   it('should return 0 when the value is a blank string', function() {
+//     var result = Calculator.add('');
+//
+//     assert.equal(0, result);
+//   });
+//
+//   it('should return 3 when the value is 3', function() {
+//     var result = Calculator.add('3');
+//
+//     assert.equal(3, result);
+//   });
+//
+//   it('should return 5 when the value is "2,3"', function() {
+//     var result = Calculator.add('2,3');
+//
+//     assert.equal(5, result);
+//   });
+//
+//   it('should return 10 when the value passed in is "5,5"', function() {
+//     var result = Calculator.add('5,5');
+//
+//     assert.equal(10, result);
+//   })
+//
+// });

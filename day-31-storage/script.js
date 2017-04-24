256cc7f2261740f4e058f069a1bfbcc65b66fbac
David Espinoza <despinoza1027@gmail.com>
2017-04-24 14:19:16 -0500

    created day-31-storage folder and files

---
+++ day-31-storage/script.js
@@ -0,0 +1,12 @@
+var clickMe = document.querySelector("#click-me");
+
+var count = Number(localStorage.getItem('click-count'));
+clickMe.innerHTML = count;
+
+clickMe.addEventListener('click', function() {
+  count += 1;
+
+  localStorage.setItem('click-count', count);
+
+  clickMe.innerHTML = count;
+});

6a1ef8c8795f2779baad8f1c7bfc428700eca132
David Espinoza <despinoza1027@gmail.com>
2017-04-26 10:42:44 -0500

    completed in-class portion

--- day-33-react-app/src/index.js
+++ day-33-react-app/src/index.js
@@ -2,13 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Clicker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      elephantTotal: 34567,
+      trucksOfCheese: 3456789876543
+    }
+  }
+
+  handleClick() {
+    var value = this.state.value + 1;
+    this.setState({
+      value: value
+    });
+  }
+
+  render() {
+    return (
+      <div onClick={ () => this.handleClick()}>
+        <strong>Value:</strong> {this.state.value}
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
 
   render() {
     return (
-      <div>hi</div>
+      <div>
+        <Clicker />
+        <Clicker />
+        <Clicker />
+        <Clicker />
+        <Clicker />
+        <Clicker />
+        <Clicker />
+      </div>
     )
   }
+
 }
 
 ReactDOM.render(

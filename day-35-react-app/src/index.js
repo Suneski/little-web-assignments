3fe98bca717a4c458a6ac6d036861964af3e4ad5
David Espinoza <despinoza1027@gmail.com>
2017-04-28 09:39:54 -0500

    completed warmup

--- day-35-react-app/src/index.js
+++ day-35-react-app/src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+class Toggle extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "on"
+    }
+  }
+
+  toggler() {
+    if (this.state.value === "on") {
+      this.setState({
+        value: "off"
+      })
+    }
+    if (this.state.value === "off") {
+      this.setState({
+        value: "on"
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={ () => this.toggler()}>
+        {this.state.value}
+      </div>
+    )
+
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );

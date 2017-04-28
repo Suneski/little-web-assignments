78aa947891ac05fd955a92867cbd46570afe003d
David Espinoza <despinoza1027@gmail.com>
2017-04-28 14:54:24 -0500

    uploaded Eric's notes

--- day-35-react-app/src/index.js
+++ day-35-react-app/src/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SwapiPeople from './SwapiPeople.js'
-import Toggler from './Toggler.js'
+// import SwapiPeople from './SwapiPeople.js'
+// import Toggler from './Toggler.js'
+import Clock from './Clock.js'
+import SuperClock from './SuperClock.js'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Toggler />,
-        <SwapiPeople />
+        <Clock />
+        <SuperClock />
       </div>
     );
   }

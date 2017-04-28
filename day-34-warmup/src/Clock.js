406ea1d8db1d7f48ad068d98b0660a9b4878ffa1
David Espinoza <despinoza1027@gmail.com>
2017-04-27 22:20:10 -0500

    updated clock.js

--- day-34-warmup/src/Clock.js
+++ day-34-warmup/src/Clock.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-//
-//
-// class Clock extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       time: new Date().toTimeString()
-//     }
-//   }
-//
-//   clock() {
-//     this.setState({
-//       time: new Date().toLocaleTimeString()
-//     });
-//   }
-//
-//   componentDidMount() {
-//     setInterval(() => this.clock(), 1000);
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         {this.state.time}
-//       </div>
-//     )
-//   }
-// }
-//
-// module.exports = Clock;
-
-
-
-
 import React from 'react';
 
 
@@ -40,21 +5,14 @@ class Clock extends React.Component {
   constructor() {
     super();
     this.state = {
-      hour: new Date().getHours(),
-      minute: new Date().getMinutes(),
-      seconds: new Date().getSeconds()
+      time: new Date().toTimeString()
     }
   }
 
   clock() {
-    var newDate = new Date();
-
     this.setState({
-      hour: newDate.getHours(),
-      minute: newDate.getMinutes(),
-      seconds: newDate.getSeconds()
-    })
-
+      time: new Date().toLocaleTimeString()
+    });
   }
 
   componentDidMount() {
@@ -64,9 +22,7 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        <div className="boringClock">
-          {this.state.hour}:{this.state.minute}:{this.state.seconds}
-        </div>
+        {this.state.time}
       </div>
     )
   }

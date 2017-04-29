c0c39cff1b170d345ef4e19b88f2e41fa13ca828
David Espinoza <despinoza1027@gmail.com>
2017-04-29 16:27:18 -0500

    created basic API pull on 'takoyaki'

--- day-35-github-repo-search-react/src/App.js
+++ day-35-github-repo-search-react/src/App.js
@@ -6,12 +6,14 @@ import RepoSearch from './RepoSearch';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Github Repository Search</h2>
+      <div>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Github Repository Search</h2>
+          </div>
         </div>
-        <RepoSearch />
+        <RepoSearch />      
       </div>
     );
   }
--- day-35-github-repo-search-react/src/RepoSearch.js
+++ day-35-github-repo-search-react/src/RepoSearch.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import './App.css';
+import $ from 'jquery';
+import './reposearch.css';
+
+class Takoyaki extends React.Component {
+  render() {
+    return(
+      <li>
+        <div>Repo Name: {this.props.name}</div>
+        <div>User: {this.props.login}</div>
+      </li>
+    )
+  }
+}
 
 class RepoSearch extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      repos: []
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'https://api.github.com/search/repositories?q=takoyaki&page=1'
+    })
+    .done((data) => {
+      console.log('Is this working!?', data);
+      this.setState({
+        repos: data.items
+      })
+    });
+  }
+
   render() {
+    let repos = this.state.repos.map((x) => <Takoyaki key={x.url} name={x.name} login={x.owner.login}/>);
     return (
       <div>
-        <p>Hi</p>
+        <ul>
+          {repos}
+        </ul>
       </div>
     );
   }

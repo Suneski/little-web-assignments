d77529e2def58c271157936bfb5cd4aaf5046041
David Espinoza <despinoza1027@gmail.com>
2017-05-01 11:23:31 -0500

    updated index.js

--- day-36-react-app/src/index.js
+++ day-36-react-app/src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import SimpleList from './SimpleList';
 import './index.css';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-div">
+        <SimpleList />
+        <SimpleList />
+        <SimpleList />
+        <SimpleList />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <SimpleList />,
+  <App />,
   document.getElementById('root')
 );

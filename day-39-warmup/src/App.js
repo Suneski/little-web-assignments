a55a5c9a423e0210292896c41f5dbfcbef4a1364
David Espinoza <despinoza1027@gmail.com>
2017-05-04 09:27:02 -0500

    created folder and initial files

---
+++ day-39-warmup/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+
+var Home = () => (
+  <div>Homepage (first)</div>
+)
+
+var SecondPage = () => (
+  <div>Hey, I'm a second page.</div>
+)
+
+var ThirdPage = () => (
+  <div>I'm the third page.</div>
+)
+
+var Navigation = () => {
+  return <div className='nav'>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="secondpage">Second Page</Link></li>
+      <li><Link to="thirdpage">Third Page</Link></li>
+    </ul>
+  </div>
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navigation />
+          <div className="bufferzone"></div>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/secondpage" component={SecondPage} />
+            <Route path="/thirdpage" component={ThirdPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;

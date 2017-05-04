4bb6aa7291334b5255c0cb74b1ecf497b5b6b814
David Espinoza <despinoza1027@gmail.com>
2017-05-04 10:54:05 -0500

    got list working

---
+++ day-39-routed-app/src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class List extends React.Component {
+
+  render() {
+    return (
+      <div>Je suis "the list"</div>
+    );
+  }
+}
+
+module.exports = List;
--- day-39-routed-app/src/index.js
+++ day-39-routed-app/src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import List from './List.js'
+import Detail from './Detail.js'
+import About from './About.js'
+
+class App extends React.Component {
+  render() {
+    return(
+      <Router>
+        <div>
+          <nav>
+            <ol>
+              <li><Link to="/">List</Link></li>
+              <li><Link to="/detail">Detail</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ol>
+          </nav>
+          <Route path="/" exact component={List} />
+          <Route path="/detail" component={Detail} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    )
+  }
+}
 
 ReactDOM.render(
   <App />,

e0cddf706980822b7c65da36c31a10debca62023
David Espinoza <despinoza1027@gmail.com>
2017-05-08 11:13:50 -0500

    created Counter

--- day-41-react-samples/src/Counter.js
+++ day-41-react-samples/src/Counter.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import { createStore } from 'redux';
 
-const INCREMENT = { type: 'INCREMENT' };
-const DECREMENT = { type: 'DECREMENT' };
+var constants = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT'
+};
+
+const INCREMENT = { type: 'constants.INCREMENT' };
+const DECREMENT = { type: 'constants.DECREMENT' };
+
+const counterReducer = (state = { number: 0 }, action) => {
+
+  switch(action.type) {
+    case 'constants.INCREMENT':
+      return { number: state.number + 1 };
+    case 'constants.DECREMENT':
+      return { number: state.number - 1 };
+  }
+
+  return state;
+};
+
+var store = createStore(counterReducer);
+
+// var output = counterReducer({ number: 0 }, INCREMENT);
+// console.log('output', output);
+//
+// output = counterReducer(output, DECREMENT);
+// console.log('now?', output);
+
 
 class Counter extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      number: 0
-    }
+    this.state = store.getState();
+  }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState(store.getState());
+    });
   }
 
   goDown() {
-    this.setState({
-      number: this.state.number - 1
-    })
+    store.dispatch(DECREMENT);
+    // this.setState({
+    //   number: this.state.number - 1
+    // })
   }
 
   goUp() {
-    this.setState({
-      number: this.state.number + 1
-    })
+    store.dispatch(INCREMENT);
+    // this.setState({
+    //   number: this.state.number + 1
+    // })
   }
 
   render() {

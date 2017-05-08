43a4cf114aa315735f08a71a4f142e6ee4d53a86
David Espinoza <despinoza1027@gmail.com>
2017-05-08 11:37:43 -0500

    moved store to Store.js

--- day-41-react-samples/src/Counter.js
+++ day-41-react-samples/src/Counter.js
@@ -1,38 +1,5 @@
 import React from 'react';
-import { createStore } from 'redux';
-
-var constants = {
-  INCREMENT: 'INCREMENT',
-  DECREMENT: 'DECREMENT',
-  RESET: 'RESET'
-};
-
-const INCREMENT = { type: constants.INCREMENT };
-const DECREMENT = { type: constants.DECREMENT };
-const RESET = { type: constants.RESET };
-
-const counterReducer = (state = { number: 0 }, action) => {
-
-  switch(action.type) {
-    case constants.INCREMENT:
-      return { number: state.number + 1 };
-    case constants.DECREMENT:
-      return { number: state.number - 1 };
-    case constants.RESET:
-      return { number: 0 };
-  }
-
-  return state;
-};
-
-var store = createStore(counterReducer);
-
-// var output = counterReducer({ number: 0 }, INCREMENT);
-// console.log('output', output);
-//
-// output = counterReducer(output, DECREMENT);
-// console.log('now?', output);
-
+import { store, DECREMENT, INCREMENT, RESET } from './Store.js';
 
 class Counter extends React.Component {
   constructor() {
@@ -47,31 +14,13 @@ class Counter extends React.Component {
     });
   }
 
-  goDown() {
-    store.dispatch(DECREMENT);
-    // this.setState({
-    //   number: this.state.number - 1
-    // })
-  }
-
-  goUp() {
-    store.dispatch(INCREMENT);
-    // this.setState({
-    //   number: this.state.number + 1
-    // })
-  }
-
-  resetButton() {
-    store.dispatch(RESET);
-  }
-
   render() {
     return (
       <div>
-        <button onClick={() => this.goDown()}>-</button>
+        <button onClick={() => store.dispatch(DECREMENT)}>-</button>
         <div>{this.state.number}</div>
-        <button onClick={() => this.goUp()}>+</button>
-        <button onClick={() => this.resetButton()}>RESET</button>
+        <button onClick={() => store.dispatch(INCREMENT)}>+</button>
+        <button onClick={() => store.dispatch(RESET)}>NUKE IT</button>
       </div>
     );
   }
---
+++ day-41-react-samples/src/Store.js
@@ -0,0 +1,38 @@
+import { createStore } from 'redux';
+
+// CONSTANTS
+var constants = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  RESET: 'RESET'
+};
+
+// ACTIONS
+const INCREMENT = { type: constants.INCREMENT };
+const DECREMENT = { type: constants.DECREMENT };
+const RESET = { type: constants.RESET };
+
+// REDUCER
+const counterReducer = (state = { number: 0 }, action) => {
+
+  switch(action.type) {
+    case constants.INCREMENT:
+      return { number: state.number + 1 };
+    case constants.DECREMENT:
+      return { number: state.number - 1 };
+    case constants.RESET:
+      return { number: 0 };
+  }
+
+  return state;
+};
+
+// STORE
+var store = createStore(counterReducer);
+
+module.exports = {
+  store: store,
+  DECREMENT: DECREMENT,
+  INCREMENT: INCREMENT,
+  RESET: RESET
+};

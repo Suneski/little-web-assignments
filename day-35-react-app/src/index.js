c36c8f37c31a15a4710cc155644b6ca845cb5faa
David Espinoza <despinoza1027@gmail.com>
2017-05-15 17:26:29 -0500

    uncommented components

--- day-35-react-app/src/index.js
+++ day-35-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import SwapiPeople from './SwapiPeople.js'
-// import Toggler from './Toggler.js'
+import SwapiPeople from './SwapiPeople.js'
+import Toggler from './Toggler.js'
 import Clock from './Clock.js'
 import SuperClock from './SuperClock.js'
 
@@ -13,6 +13,8 @@ class App extends React.Component {
       <div>
         <Clock />
         <SuperClock />
+        <Toggler />
+        <SwapiPeople />
       </div>
     );
   }
